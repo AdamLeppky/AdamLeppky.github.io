5b8d536c037a947b9e25ba69b6c4a6e9b31b11a1
Adam Leppky <adamleppky@yahoo.com>
2023-03-10 18:16:27 -0600

    Improve Color Schemes
    * Remove old index file.
    
    * Add useful links to readme.
    
    * Change how color schemes are done.
    
    * Improve how feedback rating is retrieved.
    
    * Remove gradient color scheme.
    
    * Add more colors to dark and light mode.

--- scripts/feedback-form.js
+++ scripts/feedback-form.js
@@ -2,18 +2,7 @@
 function onFeedbackSubmit()
 {
     console.log('Feedback processing...');
-    var rating = ""
-    if (document.getElementById("rating-1").checked) {
-        rating = "1";
-    } else if (document.getElementById("rating-2").checked) {
-        rating = "2";
-    } else if (document.getElementById("rating-3").checked) {
-        rating = "3";
-    } else if (document.getElementById("rating-4").checked) {
-        rating = "4";
-    } else if (document.getElementById("rating-5").checked) {
-        rating = "5";
-    }
+    var rating = getRating(5);
 
     var email = document.getElementById("email").value;
     var emailValid = checkEmailValid(email);
@@ -28,6 +17,16 @@ function onFeedbackSubmit()
 
     alert("Rating: " + rating + ". Email is " + emailValidMessage + ". Message word count: " + messageWordCount);
     console.log('Feedback submitted.');
+}
+
+function getRating(ratingCount) {
+    for (var i = 1; i < ratingCount; i++) {
+        var className = "rating-" + i;
+        if (document.getElementById(className).checked) {
+            return i.toString();
+        }
+    }
+    return "";
 }
 
 function checkEmailValid(address)
